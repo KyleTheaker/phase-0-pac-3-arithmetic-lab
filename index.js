2867741c76db28a69d2f26eba27f7901150d947f
Kyle <kyle.theaker@yahoo.com>
2021-02-21 02:10:50 -0800

    completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+var a, b  
+
+function add(a , b) {
+    return a += b
+};
+
+function subtract(a , b) {
+    return a -= b
+};
+
+function multiply(a , b) {
+    return a *= b
+};
+
+function divide(a , b) {
+    return a /= b
+};
+
+
+function increment(n) {
+    console.log(n++);
+    return n
+};
+
+function decrement(n) {
+    console.log(n--);
+    return n
+};
+
+function makeInt(string){
+    return parseInt(string , 10)
+};
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
